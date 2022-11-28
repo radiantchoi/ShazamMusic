9174caa219c0b9c5f676d1a45d4cffb5a6b539b1
Gordon Choi <thefirate@gmail.com>
2022-11-29 00:52:03 +0900

    UI 요소 생성

--- ShazamMusic/ViewController.swift
+++ ShazamMusic/ViewController.swift
@@ -7,13 +7,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import SnapKit
 
+final class ViewController: UIViewController {
+    private lazy var mainStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.backgroundColor = .systemGray
+        stackView.distribution = .equalSpacing
+        stackView.contentMode = .center
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    private lazy var iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "mic.circle.fill")
+        return imageView
+    }()
+    
+    private lazy var searchButton: UIButton = {
+        let button = UIButton(type: .roundedRect)
+        button.setTitle("Search", for: .normal)
+        return button
+    }()
+    
+    private lazy var infoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Try!"
+        return label
+    }()
+    
+    private lazy var playButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupView()
     }
 
-
+    private func setupView() {
+        view.addSubview(mainStackView)
+        mainStackView.snp.makeConstraints {
+            $0.edges.equalTo(view)
+                .offset(32)
+                .inset(32)
+        }
+        
+        setupStackView()
+    }
+    
+    private func setupStackView() {
+        mainStackView.addArrangedSubview(iconImageView)
+        iconImageView.snp.makeConstraints {
+            $0.height.equalTo(300)
+        }
+        
+        mainStackView.addArrangedSubview(searchButton)
+        
+        mainStackView.addArrangedSubview(infoLabel)
+        infoLabel.snp.makeConstraints {
+            $0.centerX.equalTo(mainStackView)
+        }
+        
+        mainStackView.addArrangedSubview(playButton)
+    }
 }
 
