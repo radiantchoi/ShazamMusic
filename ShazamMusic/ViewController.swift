12059972ee946596dabcc46f537482f8e5257ebc
Gordon Choi <thefirate@gmail.com>
2022-11-28 23:04:28 +0900

    Initial Commit

---
+++ ShazamMusic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShazamMusic
+//
+//  Created by Gordon Choi on 2022/11/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
